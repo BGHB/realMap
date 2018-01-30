602bdc32d61c474a7a92b868dab0010382486d69
BGHB <musthan@126.com>
2018-01-30 16:49:51 +0800

    初始化程序独立、稳定，50ms左右，接下来优化运行程序

--- realMap/camera.h
+++ realMap/camera.h
@@ -10,11 +10,20 @@ public:
 	VideoCapture            cap[6];
 
 	vector<string>          m_camPath;
-	vector <Point2f>        m_pointList;
-	vector <Point2f>        m_pointListTrans;
+	vector<Point2f>         m_pointList;
+	vector<Point2f>         m_pointListTrans;
 	vector<Mat>             m_transformList;
 	
+	vector<VideoCapture>    m_capList;
+	vector<Point2i>         m_frameSizeList;
+
+ 	const static int        imgHeight = 1080;
+	const static int        imgWidth = 1920;
 	double                  m_imgScale = 1;
+	
+	int m_key = 0;
+	char* filename = "sdf";
+
 	vector<Mat>             m_camMask;
 	vector<Mat>             m_camFrame;
 	vector<Mat>             m_camCurrrentFrame;
@@ -24,12 +33,15 @@ public:
 
 
 	void                    init();
+	void                    init_generate();
+	vector<Point2f>         getPointListTrans(vector<Point2f> pointList, vector <Mat> transformList);
+	vector<Mat>             getTransformList(vector <Point2f> pointList, vector <Point2f> pointListTrans);
+	Mat 			        getTransMat(vector<Mat> transformList);
+	void                    saveTransformList();
 	
-	int m_key = 0;
-	char * filename = "6transform.ini";
-
+	
 	void inittt(vector<Mat> & m_transformList);
-	void saveTransform(char * fileName);
+	
 	void warpMat(Mat & src, Mat & result, Mat transform, Mat mask);
 	Mat drawCross(Mat & img, int distance = 100);
 	static void on_mouse(int event, int x, int y, int flags, void* ustc);
--- realMap/main.cpp
+++ realMap/main.cpp
@@ -10,7 +10,7 @@ using namespace cv;
 
 //查表法20181025
 //读取映射矩阵 去重拼接显示
-#if 1
+#if 0
 void thread_task(string camPath, list<Mat> &camFrameList, int i) {
 	VideoCapture cap;
 	Mat img;
@@ -78,16 +78,14 @@ int main() {
 				uchar *resultData = hCamera.result.ptr<uchar>(j);
 				double *transMatData = hCamera.transMat.ptr<double>(j);
 				for (int i = 0; i < hCamera.result.cols; i++)
-				{
-					
+				{					
 					if (transMatData[4*i] < 1200000.)
 					{
 						uchar *srcData = hCamera.m_camCurrrentFrame[transMatData[4 * i+1]].ptr<uchar>(transMatData[4 * i + 2]);				
 						resultData[3 * i] = srcData[3 * (uint)transMatData[4 * i + 3]];
 						resultData[3 * i+ 1] = srcData[3 * (uint)transMatData[4 * i + 3] + 1];
 						resultData[3 * i +2] = srcData[3 * (uint)transMatData[4 * i + 3] + 2];
-					}
-					
+					}					
 				}
 			}
 
@@ -107,7 +105,6 @@ int main() {
 		{
 			Sleep(3);
 		}
-		
 	}
 	return 0;
 }
@@ -115,20 +112,16 @@ int main() {
 #endif
 
 
-//通过config.xml transMartix.xml
-#if 0
-void thread_task(string camPath, list<Mat> &camFrameList, int i) {
-	VideoCapture cap;
+//生成config.xml
+#if 1
+void thread_task(VideoCapture &cap, list<Mat> &camFrameList) {
 	Mat img;
-	cap.open(camPath);
-	if (!cap.isOpened())
-		cout << "cam " << i << " opened failed!" << endl;
 	while (true)
 	{
 		cap >> img;
 		if (!img.empty())
 		{
-			camFrameList.push_back(img);
+			camFrameList.push_back(img);	
 		}
 		this_thread::sleep_for(2ms);
 	}
@@ -137,36 +130,26 @@ void thread_task(string camPath, list<Mat> &camFrameList, int i) {
 
 int main() {
 	Camera hCamera;
-	hCamera.init();
-
+	hCamera.init_generate();
+	
 	thread threads;
-	for (int i = 0; i < 6; i++)
+	for (int i = 0; i < hCamera.m_camPath.size(); i++)
 	{
-		threads = thread(thread_task, hCamera.m_camPath[i], ref(hCamera.m_camFrameList[i]), i);
+		threads = thread(thread_task, ref(hCamera.m_capList[i]), ref(hCamera.m_camFrameList[i]));
 		threads.detach();
 	}
 
-	int result_w = 1920;
-	int result_h = 1080;
-	Mat resultForShow;
-	Mat result;
-	result.create(result_h - 200, result_w - 160, CV_8UC3);
-	Mat transMat;
-	//距离，序号，列，行（dist, num, x, y）
-	transMat.create(result.rows, result.cols, CV_64FC4);
-
 	double tstart = cvGetTickCount();
-	cout << "                       耗时：" << (cvGetTickCount() - tstart) / cvGetTickFrequency() / 1000 << "ms" << endl;
-	tstart = cvGetTickCount();
-	FileStorage readfile("transMatrix.xml", FileStorage::READ);
-	readfile["transMatrix"] >> transMat;
-	readfile.release();
+	hCamera.transMat = hCamera.getTransMat(hCamera.m_transformList);
 	//namedWindow("result", 1);
 	while (true)
 	{
 		int flag = 0;
 		for (int i = 0; i < hCamera.m_camNum; i++) {
 			cout << hCamera.m_camFrameList[i].size() << "  ";
+			//if (30 < hCamera.m_camFrameList[i].size()){
+			//	hCamera.m_camFrameList[i].clear();
+			//}
 			if (0 < hCamera.m_camFrameList[i].size()) {
 				flag++;
 			}
@@ -177,119 +160,52 @@ int main() {
 			for (int k = 0; k < hCamera.m_camNum; k++)
 			{
 				hCamera.m_camCurrrentFrame[k] = hCamera.m_camFrameList[k].back().clone();
-				hCamera.m_camFrameList[k].pop_front();
+				
+				//hCamera.m_camFrameList[k].pop_front();
+				
 			}
-			//int r = transMat.rows;
-			//int c = transMat.cols * transMat.channels();
-			//if (transMat.isContinuous()) {
-			//	r = 1;
-			//	c = transMat.rows * transMat.cols * transMat.channels();
-			//}
-			//for (int j = 0; j < r; j++)
-			//{
-			//	double *data = transMat.ptr<double>(j);
-			//	for (int i = 0; i < c; i++)
-			//	{
-			//		data[i] = 1440000.;
-			//	}
-			//}
-			//
-			//
-			//for (int k = 0; k < hCamera.m_camNum; k++)
-			//{
-			//	hCamera.m_camCurrrentFrame[k] = hCamera.m_camFrameList[k].back().clone();
-			//	hCamera.m_camFrameList[k].pop_front();
-			//	
-			//	//hCamera.warpMat(hCamera.m_camCurrrentFrame[i], result, hCamera.m_transformList[i], hCamera.m_camMask[i]);
-			//	//void Camera::warpMat(Mat & src, Mat & result, Mat transform, Mat mask)
-			//	double * px0 = hCamera.m_transformList[k].ptr<double>(0);
-			//	double * px1 = hCamera.m_transformList[k].ptr<double>(1);
-			//	double * px2 = hCamera.m_transformList[k].ptr<double>(2);
-			//	//cout << transform << endl;
-			//	int transRow, transCol;
-			//	for (int j = 0; j < hCamera.m_camCurrrentFrame[k].rows; j++)
-			//	{
-			//		uchar * srcData = hCamera.m_camCurrrentFrame[k].ptr<uchar>(j);
-			//		for (int i = 0; i < hCamera.m_camCurrrentFrame[k].cols; i++)
-			//		{
-			//			transCol = (i * px0[0] + j * px0[1] + px0[2]) / (i * px2[0] + j * px2[1] + px2[2]);
-			//			transRow = (i * px1[0] + j * px1[1] + px1[2]) / (i * px2[0] + j * px2[1] + px2[2]);
-			//			//cout << resultRow<<", "<< resultCol << endl;
-			//			if (transRow >= 0 && transRow < transMat.rows && transCol >= 0 && transCol < transMat.cols)
-			//			{
-			//				double *transMatData = transMat.ptr<double>(transRow);
-			//				
-			//				int dist = (i - hCamera.m_camCurrrentFrame[k].cols / 2)*(i - hCamera.m_camCurrrentFrame[k].cols / 2)
-			//					+ (j - hCamera.m_camCurrrentFrame[k].rows / 2)*(j - hCamera.m_camCurrrentFrame[k].rows / 2);
-			//				if (dist < transMatData[4 * transCol])
-			//				{
-			//					transMatData[4 * transCol] = dist;
-			//					transMatData[4 * transCol + 1] = k;
-			//					transMatData[4 * transCol + 2] = j;
-			//					transMatData[4 * transCol + 3] = i;
-			//					//cout << transMatData[4 * transCol]<< "   "<<transMatData[4 * transCol+1] <<"   "
-			//					//	<<transMatData[4 * transCol+2] <<"   "<< transMatData[4 * transCol+3]  << endl;
-			//				}
-			//			}
-			//		}
-			//	}
-			//}
-
-			//FileStorage writefile("transMatrix.xml", FileStorage::WRITE);
-			//writefile << "transMatrix" << transMat;
-			//writefile.release();
-
-			if (result.isContinuous()) {
-				memset(result.data, 0, result.rows*result.cols*result.channels());
+			if (hCamera.result.isContinuous()) {
+				memset(hCamera.result.data, 0, hCamera.result.rows * hCamera.result.cols * hCamera.result.channels());
 			}
-			for (int j = 0; j < result.rows; j++)
+
+			for (int j = 0; j < hCamera.result.rows; j++)
 			{
-				uchar *resultData = result.ptr<uchar>(j);
-				double *transMatData = transMat.ptr<double>(j);
-				for (int i = 0; i < result.cols; i++)
+				uchar *resultData = hCamera.result.ptr<uchar>(j);
+				double *transMatData = hCamera.transMat.ptr<double>(j);
+				for (int i = 0; i < hCamera.result.cols; i++)
 				{
-
 					if (transMatData[4 * i] < 1200000.)
 					{
-
 						uchar *srcData = hCamera.m_camCurrrentFrame[transMatData[4 * i + 1]].ptr<uchar>(transMatData[4 * i + 2]);
-						if (j == 336 && i > 1237)
-						{
-							cout << transMatData[4 * i + 3] << endl;
-							cout << 3 * (uint)transMatData[4 * i + 3] << endl;
-							cout << (int)srcData[3 * (uint)transMatData[4 * i + 3]] << endl;
-						}
+						//uchar *srcData = hCamera.m_camCurrrentFrame[0].ptr<uchar>(transMatData[4 * i + 2]);
 
 						resultData[3 * i] = srcData[3 * (uint)transMatData[4 * i + 3]];
 						resultData[3 * i + 1] = srcData[3 * (uint)transMatData[4 * i + 3] + 1];
 						resultData[3 * i + 2] = srcData[3 * (uint)transMatData[4 * i + 3] + 2];
 					}
-
 				}
+				//cout <<"("<< j << endl;
 			}
 
 			cout << "                       耗时：" << (cvGetTickCount() - tstart) / cvGetTickFrequency() / 1000 << "ms" << endl;
 			tstart = cvGetTickCount();
-			//for (int k = 0; k < hCamera.m_pointList.size(); k++)
-			//{
-			//circle(result, hTransform.m_pointList[k], 4, Scalar(0, 0, 255), -1);
-			//circle(result, hCamera.m_pointListTrans[k], 4, Scalar(0, 255 - k / 4 * 255 / 6, k / 4 * 255 / 6), -1);
-			//}
-			result = hCamera.drawCross(result, 80);
-			//setMouseCallback("result", Camera::on_mouse, (void *)&hCamera);
-			imshow("result", result);
+			for (int k = 0; k < hCamera.m_pointList.size(); k++)
+			{
+				//circle(result, hTransform.m_pointList[k], 4, Scalar(0, 0, 255), -1);
+				circle(hCamera.result, hCamera.m_pointListTrans[k], 4, Scalar(0, 255 - k / 4 * 255 / 6, k / 4 * 255 / 6), -1);
+			}
+			hCamera.result = hCamera.drawCross(hCamera.result, 80);
+			setMouseCallback("result", Camera::on_mouse, (void *)&hCamera);
+			imshow("result", hCamera.result);
 			waitKey(1);
 		}
 		else
 		{
 			Sleep(3);
 		}
-
 	}
 	return 0;
 }
-}
-
 #endif
 
 //config版本，备份，20180123
